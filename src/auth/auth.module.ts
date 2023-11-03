d2950b8706923408986d0b9df70505c928f39a89
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-11-03 15:49:59 +0100

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common"
+
+
+@Module({})
+export class AuthModule {
+
+}
+
+// initializing nest app-- \nest new name_of_app\
+// creating module from terminal: nest g module name_of_module, E.g nest g module user
+
